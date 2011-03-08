fb920b1bd66e7f9b54ae98c8ae1d8952a3ff8a04
Alex Reisner <alex@alexreisner.com>
2011-03-07 23:36:34 -0500

    Make test more specific and thorough.

--- base/test/functional/venues_controller_test.rb
+++ base/test/functional/venues_controller_test.rb
@@ -2,8 +2,10 @@ require 'test_helper'
 
 class VenuesControllerTest < ActionController::TestCase
 
-  test "request location returns Geocoder::Result" do
+  test "request location returns Geocoder::Result::Freegeoip" do
     get :index
-    assert @request.location.kind_of?(Geocoder::Result::Base)
+    assert @request.location.kind_of?(Geocoder::Result::Freegeoip)
+    assert_equal "0.0.0.0", @request.location.ip
+    assert_equal "Reserved", @request.location.country
   end
 end
